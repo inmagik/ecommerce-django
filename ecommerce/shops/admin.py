d5ba8790cb25315927bec8e105fcf5894986019f
Mauro Bianchi <bianchimro@gmail.com>
2015-11-27 11:32:57 +0100

    added admin

--- ecommerce/shops/admin.py
+++ ecommerce/shops/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Shop, ProductClass, ProductField, Product
+
+admin.site.register(Shop)
+admin.site.register(Product)
+
